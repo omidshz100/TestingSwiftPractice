e389daa77d62be9ad37eadc0f472b704fb968960
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2024-06-12 17:04:33 +0200

    Initial Commit

---
+++ TestingSwiftPractice/TestingSwiftPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingSwiftPracticeApp.swift
+//  TestingSwiftPractice
+//
+//  Created by Omid Shojaeian Zanjani on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingSwiftPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
